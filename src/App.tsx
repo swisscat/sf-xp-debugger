70a3c694062e55294357deeb9e37904705420427
swisscat <swisscatman@gmail.com>
2022-09-20 10:11:26 +0200

    Formatted requests per URL for cleaner interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,63 @@
-import { Spinner, Toast, ToastContainer } from "@salesforce/design-system-react";
-import React, { Component } from "react";
-import { Browser } from "webextension-polyfill";
+import { Modal, Spinner, Toast, ToastContainer } from "@salesforce/design-system-react";
+import React, { Component, ReactNode } from "react";
+import { Browser, Cookies } from "webextension-polyfill";
 import DebugInformation from "./components/DebugInformation";
 import ManageTrace from "./components/ManageTrace";
-import RequestStack from "./components/RequestStack";
+import TraceExplorer from "./components/TraceExplorer";
 import Tabs from "./layout/Tabs";
 import { Connection, SfDate } from "jsforce";
 
 declare var browser: Browser;
 
-interface Props {}
+interface Props { }
 
 interface TraceFlag {
   ExpirationDate: string;
 }
 interface AppState {
-  error?: string;
-  sfApi: Connection;
+  error?: {
+    title: string;
+    message: ReactNode;
+  };
+  currentDomain?: string;
+  sfApi?: Connection;
   externalUserId?: string;
   traceActiveUntil?: Date;
 }
 
 class App extends Component<Props, AppState> {
-  state: AppState = {
-    error: undefined,
-    sfApi: new Connection({}),
-    externalUserId: undefined,
-  };
+  state: AppState = {};
   constructor(props: any) {
     super(props);
-    this.componentDidMount = this.componentDidMount.bind(this);
   }
-  async componentDidMount() {
+  cookieChangeListener = ({ cookie }: Cookies.OnChangedChangeInfoType) => {
+    if (["RRetURL", "sid"].includes(cookie.name)) {
+      this.bootstrapApp();
+    }
+  }
+  componentDidMount() {
+    this.bootstrapApp();
+
+    browser.cookies.onChanged.addListener(this.cookieChangeListener);
+  }
+  componentWillUnmount(): void {
+    browser.cookies.onChanged.removeListener(this.cookieChangeListener);
+  }
+  async bootstrapApp() {
+    this.setState({
+      error: undefined
+    })
+
     const [currentLocation] = await browser.devtools.inspectedWindow.eval("location.href");
 
     const loggedAsCookie = await browser.cookies.get({ name: "RRetURL", url: currentLocation });
 
     if (loggedAsCookie === null) {
       return this.setState({
-        error: "Salesforce domain could not be established. Please log in from Salesforce, and access Experience Cloud using Login As to use this debugger",
+        error: {
+          title: "Experience Cloud not found",
+          message: <><p>Could not find an Experience Cloud domain on <a href="#top">{currentLocation}</a>.</p><p>Please connect to Salesforce, then use "login as" a Contact in order to use this tool.</p></>
+        },
       });
     }
 
@@ -51,7 +70,10 @@ class App extends Component<Props, AppState> {
 
     if (salesforceSessionCookie === null) {
       return this.setState({
-        error: `Could not find an established session for ${instanceUrl}. Please log in again on that domain.`,
+        error: {
+          title: "Salesforce Session not found",
+          message: <><p>Could not find an established session for <a href="#top">{instanceUrl}</a>.</p><p>Please log in again on that domain.</p></>
+        },
       });
     }
 
@@ -64,13 +86,19 @@ class App extends Component<Props, AppState> {
       externalUser = await sfApi.sobject("User").findOne({ ContactId: externalContactId }, ["Id"]);
     } catch (err) {
       return this.setState({
-        error: `Invalid session or expired`,
+        error: {
+          title: 'Salesforce Session expired',
+          message: <><p>The session on <a href="#top">{instanceUrl}</a> has expired.</p><p>Please log in again on that domain.</p></>
+        },
       });
     }
 
     if (!externalUser) {
       return this.setState({
-        error: `The user with Contact ID ${externalContactId} cannot be found in the platform. It may have been removed. Please log-in again.`,
+        error: {
+          title: 'Contact not found',
+          message: <><p>The user with Contact ID ${externalContactId} cannot be found on <a href="#top">${instanceUrl}</a>. It may have been removed.</p><p>Please log-in again.</p></>
+        },
       });
     }
 
@@ -81,7 +109,7 @@ class App extends Component<Props, AppState> {
     const activeTrace = await sfApi.tooling
       .sobject("TraceFlag")
       .findOne({ TracedEntityId: externalUserId, ExpirationDate: { $gte: SfDate.toDateTimeLiteral(new Date()) } }, ["ExpirationDate"]) as TraceFlag
-      
+
     if (activeTrace) {
       traceActiveUntil = SfDate.parseDate(activeTrace.ExpirationDate);
     }
@@ -97,29 +125,22 @@ class App extends Component<Props, AppState> {
 
     if (error) {
       return (
-        <ToastContainer>
-          <Toast
-            labels={{
-              heading: error,
-            }}
-            variant="error"
-          />
-        </ToastContainer>
+        <Modal disableClose ariaHideApp={false} prompt="error" size="medium" heading={error.title} isOpen={true}><div className="slds-m-around_medium">{error.message}</div></Modal>
       );
     }
 
-    if (!externalUserId) {
+    if (!sfApi || !externalUserId) {
       return <Spinner size="large" variant="brand" />;
     }
 
     return (
       <Tabs appName="XP Profiler">
-      <div data-label="Debug">
-        <RequestStack sfApi={sfApi} traceActiveUntil={traceActiveUntil} />
-      </div>
+        <div data-label="Debug">
+          <TraceExplorer sfApi={sfApi} traceActiveUntil={traceActiveUntil} />
+        </div>
         <div data-label="Configuration">
           <div className="slds-box">
-            {externalUserId && <ManageTrace sfApi={sfApi} externalUserId={externalUserId} onTraceChange={this.componentDidMount} hasActiveTrace={!!traceActiveUntil} />}
+            {externalUserId && <ManageTrace sfApi={sfApi} externalUserId={externalUserId} onTraceChange={() => this.bootstrapApp()} hasActiveTrace={!!traceActiveUntil} />}
             <DebugInformation sfApi={sfApi} externalUserId={externalUserId} />
           </div>
         </div>
---
+++ src/components/TraceExplorer.tsx
@@ -0,0 +1,309 @@
+import { Accordion, AccordionPanel, Button, Card, DataTable, DataTableCell, DataTableColumn, Icon } from "@salesforce/design-system-react";
+import { Entry } from "har-format";
+import { Connection } from "jsforce";
+import React, { Component } from "react";
+import { Browser } from "webextension-polyfill";
+import DebugFrame from "./DebugFrame";
+import { dispatchEventOnNavigation, sendMessageOnNavigationEvent } from './TraceExplorerScripts';
+declare var browser: Browser;
+
+interface Props {
+  sfApi: Connection;
+  traceActiveUntil?: Date;
+}
+
+interface Trace {
+  url: string;
+  stack: Array<SalesforceRequest>
+}
+
+interface SalesforceRequest {
+  name: string;
+  startDate: Date;
+  endDate: Date;
+  requestId: string;
+  summary: {};
+  items: Array<SalesforceRequestItem>;
+  children: Array<SalesforceRequest>;
+  logs: Array<string>;
+}
+
+interface SalesforceRequestItem {
+  id: string;
+  name: string;
+  total: number;
+  db: number;
+}
+
+interface SalesforceRequestMessage {
+  actions: Array<SalesforceRequestMessageAction>;
+}
+
+interface SalesforceRequestMessageAction {
+  id: string;
+  descriptor: string;
+  params: {
+    classname: string;
+    method: string;
+  };
+}
+
+interface SalesforceResponseBody {
+  perfSummary: {
+    actionsTotal: {};
+    actions: {
+      [action: string]: {
+        total: number;
+        db: number;
+      };
+    };
+  };
+}
+
+interface BrowserRequest extends Entry {
+  getContent: Function;
+}
+
+interface State {
+  traceList: Array<Trace>;
+  showLog?: {
+    requestId: string;
+    itemId: string;
+  };
+  inspectDate?: Date;
+}
+
+const allNodes = (stack: Array<SalesforceRequest>): Array<SalesforceRequest> => {
+  return ([] as Array<SalesforceRequest>).concat.apply(
+    [],
+    stack.map((item) => {
+      if (item.children.length) {
+        return [...allNodes(item.children), item];
+      }
+      return [item];
+    })
+  );
+};
+
+export default class TraceExplorer extends Component<Props> {
+  state: State = {
+    traceList: [],
+  };
+  constructor(props: Props) {
+    super(props);
+
+    this.requestListener = this.requestListener.bind(this);
+    this.reloadListener = this.reloadListener.bind(this);
+    this.messageListener = this.messageListener.bind(this);
+  }
+  requestListener = async (request: unknown) => {
+    const { traceActiveUntil, sfApi } = this.props;
+    const { traceList } = this.state;
+    const entry = request as Entry;
+    const endDate = new Date();
+    const trace = traceList[traceList.length - 1];
+
+    const messageParam = entry.request.postData?.params?.find((param) => param.name === "message")?.value;
+    const requestIdHeader = entry.request.headers.find((header) => header.name.toLowerCase() === "x-sfdc-request-id")?.value;
+
+    if (entry.request.url.indexOf("s/sfsites/aura") === -1 || !entry.request.queryString.find((param) => param.name === "aura.ApexAction.execute")) {
+      return;
+    }
+
+    if (!messageParam || !requestIdHeader) {
+      console.error("Unexpected request", entry.request);
+      return;
+    }
+
+    let body: SalesforceResponseBody;
+
+    try {
+      const [rawBody] = await (request as BrowserRequest).getContent();
+      body = JSON.parse(rawBody);
+    } catch (err) {
+      console.error("Caught unexpected request with non-JSON body", entry.request);
+      return;
+    }
+
+    const message: SalesforceRequestMessage = JSON.parse(decodeURIComponent(messageParam));
+
+    const item: SalesforceRequest = {
+      name: "APEX",
+      startDate: new Date(entry.startedDateTime),
+      endDate,
+      requestId: requestIdHeader,
+      summary: body.perfSummary.actionsTotal,
+      items: message.actions
+        .filter((action) => action.descriptor === "aura://ApexActionController/ACTION$execute")
+        .map((action) => ({
+          id: action.id,
+          name: `${action.params.classname}.${action.params.method}`,
+
+          ...body.perfSummary.actions[action.id],
+        })),
+      children: [],
+      logs: [],
+    };
+
+    if (!!traceActiveUntil) {
+      const logs = (await sfApi.sobject("ApexLog").find({ RequestIdentifier: `TID:${requestIdHeader}` }, ["Id"])) as Array<{ Id: string }>;
+      item.logs = logs.map((item) => item.Id);
+    }
+
+    const finishedRequests = allNodes(trace.stack).filter((req) => req.endDate < item.startDate);
+
+    let stackRequest;
+    if (finishedRequests.length) {
+      stackRequest = finishedRequests.reduce((a, b) => {
+        return a.endDate > b.endDate ? a : b;
+      });
+    }
+
+    if (stackRequest) {
+      stackRequest.children.push(item);
+    } else {
+      trace.stack.push(item);
+    }
+
+    this.setState({
+      traceList,
+    });
+
+    browser.storage.local.set({ traceList });
+  };
+  reloadListener() {
+    this.initUrlChangeListener();
+    this.initTraceList();
+  }
+  messageListener(message: any) {
+    if (message.type === 'urlChange') {
+      this.initTraceList();
+    }
+  }
+  async initTraceList() {
+    const { traceList: storedTraceList } = await browser.storage.local.get("traceList");
+
+    const traceList = storedTraceList || [];
+
+    const [currentLocation] = await browser.devtools.inspectedWindow.eval("location.href.substring(location.origin.length)");
+
+    traceList.push({
+      url: currentLocation,
+      stack: []
+    })
+
+    this.setState({ traceList });
+  }
+  initUrlChangeListener(initRequestListener: boolean = false) {
+    if (initRequestListener) {
+      browser.devtools.network.onRequestFinished.addListener(this.requestListener);
+    }
+    browser.devtools.network.onNavigated.addListener(this.reloadListener);
+    browser.runtime.onMessage.addListener(this.messageListener);
+
+    browser.devtools.inspectedWindow.eval(`(${dispatchEventOnNavigation.toString()})()`);
+
+    browser.scripting.executeScript({
+      target: { tabId: browser.devtools.inspectedWindow.tabId },
+      func: sendMessageOnNavigationEvent
+    });
+  }
+  async componentDidMount() {
+    this.initTraceList();
+    this.initUrlChangeListener(true);
+  }
+
+  componentWillUnmount(): void {
+    browser.devtools.network.onRequestFinished.removeListener(this.requestListener);
+    browser.devtools.network.onNavigated.removeListener(this.reloadListener);
+    browser.runtime.onMessage.removeListener(this.messageListener);
+  }
+
+  flushStack() {
+    browser.storage.local.remove("traceList");
+    this.initTraceList();
+  }
+
+  renderStackChildren(stack: Array<SalesforceRequest>) {
+    const { sfApi } = this.props;
+    const { showLog, inspectDate } = this.state;
+
+    const ViewTraceTableCell = ({ ...props }) => (
+      <DataTableCell {...props}>
+        <a
+          href="#cell"
+          onClick={() =>
+            this.setState({
+              showLog: {
+                requestId: props.request,
+                itemId: props.item.id,
+              },
+              inspectDate: new Date(),
+            })
+          }
+        >
+          View Trace
+        </a>
+      </DataTableCell>
+    );
+    ViewTraceTableCell.displayName = DataTableCell.displayName;
+    return (
+      <div className="slds-box">
+        <div className="slds-grid slds-gutters">
+          {stack.map((item, idx) => (
+            <div className="slds-col" key={item.requestId + idx}>
+              <Card heading={`APEX: ${item.items.length} requests`} icon={<Icon category="standard" name="apex" size="small" />}>
+                <DataTable items={item.items}>
+                  <DataTableColumn key="name" label="Controller" property="name" />
+                  <DataTableColumn key="total" label="Total" property="total" />
+                  <DataTableColumn key="db" label="Database" property="db" />
+                  {!!item.logs.length &&
+                    <DataTableColumn key="id" label="" property="id">
+                      <ViewTraceTableCell request={item.requestId} logs={item.logs} />
+                    </DataTableColumn>}
+                </DataTable>
+                {inspectDate &&
+                  item.items.map((rq) => (
+                    <DebugFrame
+                      key={item.requestId + rq.id}
+                      logs={item.logs}
+                      apexName={rq.name}
+                      inspectDate={showLog && showLog.requestId === item.requestId && showLog.itemId === rq.id ? inspectDate : undefined}
+                      sfApi={sfApi}
+                    />
+                  ))}
+                {!!item.children.length && (
+                  <div className="slds-card__body slds-card__body_inner">
+                    <p className="slds-text-heading_small slds-truncate slds-m-bottom_small">
+                      Sub-requests
+                    </p>
+                    {this.renderStackChildren(item.children)}
+                  </div>
+                )}
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { traceActiveUntil } = this.props;
+    const { traceList } = this.state;
+
+    return <>
+      <Accordion>
+        {traceList.map((trace, idx) => <AccordionPanel expanded={true} key={idx} summary={trace.url} id={idx} onTogglePanel={() => { }} onClickSummary={() => { }}>
+          {this.renderStackChildren(trace.stack)}
+        </AccordionPanel>).reverse()}
+      </Accordion>
+      <div className="slds-docked-form-footer">
+        <div className="slds-button slds-text-body_regular slds-text-color_weak">
+          {traceActiveUntil ? `Trace active until ${traceActiveUntil.toLocaleString()}` : `Trace not enabled`} &bull;
+        </div>
+        <Button label="Flush logs" onClick={() => this.flushStack()} />
+      </div>
+    </>
+  }
+}
